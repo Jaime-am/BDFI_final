c5c321ba454580e36b1ee6898394e05567555f0d
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2024-01-16 20:16:50 +0100

    Update cassandra-init.sh

--- docker-compose/cassandra/cassandra-init.sh
+++ docker-compose/cassandra/cassandra-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-printf "Esperando despliegue de Cassandra" && sleep 30;
+printf "Esperando despliegue de Cassandra" && sleep 60;
 echo "Creating keyspace and table..."
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS agile_data_science WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS agile_data_science.flight_delay_classification_response (Origin TEXT, DayOfWeek INT, DayOfYear INT, DayOfMonth INT, Dest TEXT, DepDelay DOUBLE, Timestamp TIMESTAMP, FlightDate DATE, Carrier TEXT, UUID UUID PRIMARY KEY, Distance INT, Route TEXT, Prediction INT);"
